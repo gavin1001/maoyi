a869dd36df075590f80098a03953431c097f3401
Administrator <Administrator@windows10.microdone.cn>
2019-07-19 16:59:33 +0800

    完善添加

--- src/main/java/com/yy/maoyi/ca/SignPdf.java
+++ src/main/java/com/yy/maoyi/ca/SignPdf.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.Security;
@@ -58,6 +59,66 @@ public class SignPdf {
 			signPDFData = new ByteArrayOutputStream();
 			// 临时pdf文件
 			File temp = new File(signPdfSrcFile.getParent(), System.currentTimeMillis() + ".pdf");
+			stp = PdfStamper.createSignature(reader, signPDFData, '\0', temp, true);
+			stp.setFullCompression();
+			PdfSignatureAppearance sap = stp.getSignatureAppearance();
+			sap.setReason("数字签名，不可改变");
+			// 使用png格式透明图片
+			Image image = Image.getInstance(signImage);
+			sap.setImageScale(0);
+			sap.setSignatureGraphic(image);
+			sap.setRenderingMode(RenderingMode.GRAPHIC);
+			// 是对应x轴和y轴坐标
+			sap.setVisibleSignature(new Rectangle(975, 40, 110, 170), 1,
+					UUID.randomUUID().toString().replaceAll("-", ""));
+			stp.getWriter().setCompressionLevel(5);
+			ExternalDigest digest = new BouncyCastleDigest();
+			ExternalSignature signature = new PrivateKeySignature(key, DigestAlgorithms.SHA512, provider.getName());
+			MakeSignature.signDetached(sap, digest, signature, chain, null, null, null, 0, CryptoStandard.CADES);
+			stp.close();
+			reader.close();
+			return signPDFData.toByteArray();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+			if (signPDFData != null) {
+				try {
+					signPDFData.close();
+				} catch (IOException e) {
+				}
+			}
+
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		return null;
+	}
+
+	public static byte[] sign(String password, String keyStorePath, InputStream signPdfSrc, String signImage, float x,
+			float y) {
+		PdfReader reader = null;
+		ByteArrayOutputStream signPDFData = null;
+		PdfStamper stp = null;
+		FileInputStream fos = null;
+		try {
+			BouncyCastleProvider provider = new BouncyCastleProvider();
+			Security.addProvider(provider);
+			KeyStore ks = KeyStore.getInstance("PKCS12", new BouncyCastleProvider());
+			fos = new FileInputStream(keyStorePath);
+			// 私钥密码 为Pkcs生成证书是的私钥密码 123456
+			ks.load(fos, password.toCharArray());
+			String alias = (String) ks.aliases().nextElement();
+			PrivateKey key = (PrivateKey) ks.getKey(alias, password.toCharArray());
+			Certificate[] chain = ks.getCertificateChain(alias);
+			reader = new PdfReader(signPdfSrc);
+			signPDFData = new ByteArrayOutputStream();
+			// 临时pdf文件
+			File temp = new File(System.getProperty("user.dir") + "/file/", System.currentTimeMillis() + ".pdf");
 			stp = PdfStamper.createSignature(reader, signPDFData, '\0', temp, true);
 			stp.setFullCompression();
 			PdfSignatureAppearance sap = stp.getSignatureAppearance();
--- src/main/java/com/yy/maoyi/web/MaoYiDown.java
+++ src/main/java/com/yy/maoyi/web/MaoYiDown.java
@@ -1,6 +1,7 @@
 package com.yy.maoyi.web;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,50 +14,74 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yy.maoyi.ca.SignPdf;
+
 @Controller
 @RequestMapping("maoyiDown")
-public class MaoYiDown extends MaoYiAction{
+public class MaoYiDown extends MaoYiAction {
 
-	
-	
 	@GetMapping("/downFile")
 	@ResponseBody
-	public void downFile(String user, String password,String queryNum, HttpServletResponse response) throws Exception {
+	public void downFile(String user, String password, String queryNum, HttpServletResponse response) throws Exception {
 
 		LoginModel status = parseLogin(user, password);
 
-		if(Objects.equals(status.getStatus(), "-1")) {
+		if (Objects.equals(status.getStatus(), "-1")) {
 			logger.info(status.getMessage());
 			return;
 		}
 		InputStream is = maoYiService.downFile(queryNum);
-		byte[] buffer = readInputStream(is);
+//		byte[] buffer = readInputStream(is);
 
-		if (null != buffer && buffer.length > 0) {
+//		File file = new File(System.getProperty("user.dir") + "/file/" + queryNum + ".pdf");
+//		if (!file.exists()) {
+//			file.createNewFile();
+//		}
+
+//		FileOutputStream fos = new FileOutputStream(System.getProperty("user.dir") + "/file/" + queryNum + ".pdf");
+//
+//		byte[] b = new byte[1024];
+//
+//		int length;
+//		while ((length = is.read(b)) > 0) {
+//			fos.write(b, 0, length);
+//		}
+//		is.close();
+//		fos.close();
+
+		byte[] fileData = SignPdf.sign("123456", System.getProperty("user.dir") + "/key/" + "keystore.p12", //
+				is, //
+				System.getProperty("user.dir") + "/icon/" + user + ".png", 100, 290);
+		if (null != fileData && fileData.length > 0) {
 			// 清空response
 			response.reset();
 			// 设置response的Header
-			response.addHeader("Content-Disposition","attachment;filename="+queryNum+".pdf");
-			response.addHeader("Content-Length", "" + buffer.length);
+			response.addHeader("Content-Disposition", "attachment;filename=" + queryNum + ".pdf");
+			response.addHeader("Content-Length", "" + fileData.length);
 			OutputStream toClient = response.getOutputStream();
 			response.setContentType("application/octet-stream");
-			toClient.write(buffer);
+			toClient.write(fileData);
 			toClient.flush();
 			toClient.close();
 		}
-	}
-	
-	
-	private byte[] readInputStream(InputStream fis) throws IOException {
-		ByteArrayOutputStream outStream = new ByteArrayOutputStream();  
-        byte[] buffer = new byte[1024];  
-        int len = 0;  
-        while( (len=fis.read(buffer)) != -1 ){  
-            outStream.write(buffer, 0, len);  
-        }  
-        fis.close();  
-        return outStream.toByteArray();
+		File file = new File(System.getProperty("user.dir") + "/file/" + queryNum + ".pdf");
 
-	}	
-	
+		if (file.exists()) {
+			file.delete();
+		}
+
+	}
+
+	protected byte[] readInputStream(InputStream fis) throws IOException {
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		while ((len = fis.read(buffer)) != -1) {
+			outStream.write(buffer, 0, len);
+		}
+		fis.close();
+		return outStream.toByteArray();
+
+	}
+
 }
